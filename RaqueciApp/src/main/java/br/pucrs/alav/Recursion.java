e70a13e395b9288494552076f4531ecbd81c8375
cristianomm <cristiano.mrmartins@gmail.com>
2022-03-08 23:35:42 -0300

    adicionado metodos

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,5 +1,52 @@
 package br.pucrs.alav;
 
+import java.util.List;
+
 public class Recursion {
+	
+	private String[] alphabet = {
+			"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+			"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+			};
+	
+	public long mult(long a, long b) {
+		return 0;
+	}
+	
+	public long sum(long a, long b) {
+		return 0;
+	}
+	
+	public double sumFraction(long n) {
+		return 0;
+	}
+	
+	public String invert(String txt) {
+		return txt;
+	}
+	
+	public long sequence(long n) {
+		return 0;
+	}
+	
+	public long ackerman(long m, long n) {
+		return 0;
+	}
+	
+	public int product(long[] values) {
+		return 0;
+	}
+	
+	public boolean isPalindrome(String word) {
+		return false;
+	}
+	
+	public List<String> alphaSequence(int n){
+		return null;
+	}
+	
+	public long fibonacci(int f0, int f1) {
+		return 0;
+	}
 	
 }
--- RaqueciApp/src/test/java/br/pucrs/alav/RecursionTests.java
+++ RaqueciApp/src/test/java/br/pucrs/alav/RecursionTests.java
@@ -10,7 +10,50 @@ public class RecursionTests {
 
 	@Test
 	public void test() { 
-		assertTrue("Ser·?", true);
+		assertTrue("Ser√°?", true);
 	}
-
+	
+	public void testMul() {
+		
+		Recursion recursion = new Recursion();
+		
+		assertEquals(56L, recursion.mult(7, 8));
+	}
+	
+	public void testSum() {
+		assertTrue(false);
+	}
+	
+	public void testCalcFrac() {
+		assertTrue(false);
+	}
+	
+	public void testInvert() {
+		assertTrue(false);
+	}
+	
+	public void testSequence() {
+		assertTrue(false);
+	}
+	
+	public void testAckerman() {
+		assertTrue(false);
+	}
+	
+	public void testVecProduct() {
+		assertTrue(false);
+	}
+	
+	public void testPalindromo() {
+		assertTrue(false);
+	}
+	
+	public void testAlphaCombination() {
+		assertTrue(false);
+	}
+	
+	public void testFibonacci() {
+		assertTrue(false);
+	}
+	
 }
